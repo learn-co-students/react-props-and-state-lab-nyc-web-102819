45ba03a73bcae0382a59c42663cd81cd58cb9a16
crpineda1 <carlos.r.pineda@gmail.com>
2020-01-12 19:11:28 -0500

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,25 +1,43 @@
 import React from 'react'
 
 class Pet extends React.Component {
+
+  state = {
+    adopted: this.props.isAdopted
+  }
+
+
+  adoptPet = (event) => {
+
+    if (event.target.textContent !== "Already Adopted") {
+      console.log("button class old", event.target.className)
+      event.target.className = "ui disabled button"
+      event.target.textContent = "Already Adopted"
+      console.log("button new class", event.target.className)
+    }
+
+  }
+
   render() {
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+            {this.props.gender === "female"? '♀': '♂'}
+            {this.props.name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.age}</p>
+            <p>Weight: {this.props.weight}</p>
           </div>
         </div>
-        <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+        <div className="extra content" >
+          {this.props.isAdopted? <button className="ui disabled button">Already adopted</button> : <button onClick = {this.adoptPet} className= "ui primary button" >Adopt pet</button>}
+          
+          
         </div>
       </div>
     )
@@ -27,3 +45,4 @@ class Pet extends React.Component {
 }
 
 export default Pet
+ 
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,9 +3,47 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      pets: [],
+      filter: null 
+    }
+    
+  }
+
+  componentDidMount() {
+    console.log("did mounnt:", this.state.pets)
+    this.setState({ 
+      pets: this.props.pets,
+      filter: this.props.filter
+    })
+  }
+
+  showPets = (pets) => pets.map( (pet) => {
+      // console.log("showpets",pet)
+      return (<Pet 
+        key = {pet.id}
+        name = {pet.name}
+        type = {pet.type}
+        gender = {pet.gender}
+        age = {pet.age}
+        weight = {pet.weight}
+        isAdopted = {pet.isAdopted}
+      />)
+      
+      }
+    ) 
+
+  
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return <div className="ui cards">
+      {this.showPets(this.props.pets)}
+    </div>
   }
 }
 
 export default PetBrowser
+ 
